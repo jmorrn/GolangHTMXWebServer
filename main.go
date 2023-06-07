3cefb66d7b556527a2fc7f4e29947110b2e9f3e2
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2023-06-07 16:45:07 -0300

    feat: Defining movie type and returning movies do template

--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 	"log"
 )
 
+type Movie struct {
+	Title string
+	Director string
+}
 
 func main(){
 	fmt.Println("Hello World")
 	
 	handlerOne := func(writer http.ResponseWriter, request *http.Request){
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(writer, nil)
+		movies := map[string][]Movie{
+			"Movies": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Pulp Fiction", Director: "Quentin Tarantino"},
+				{Title: "The Avengers", Director: "Marvel"},
+			},
+		}
+		tmpl.Execute(writer, movies)
 	}
 
 	http.HandleFunc("/", handlerOne)
